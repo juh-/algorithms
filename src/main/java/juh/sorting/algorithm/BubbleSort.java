926ba6a3827c26fe605d969b60b18373f27c7c9c
juh- <juh-@users.noreply.github.com>
2015-02-12 00:25:01 -0800

    Added SortingAlgorithmTest and BubbleSort

---
+++ src/main/java/juh/sorting/algorithm/BubbleSort.java
@@ -0,0 +1,25 @@
+package juh.sorting.algorithm;
+
+import juh.sorting.SortingAlgorithm;
+import java.util.Arrays;
+
+public class BubbleSort implements SortingAlgorithm {
+
+    public int[] sort(int[] list) {
+        int len = list.length;
+        boolean swap = true;
+        while (swap) {
+            swap = false;
+            for (int n = 0; n < len-1; n++) {
+                if (list[n] > list[n+1]) {
+                    int tmp = list[n];
+                    list[n] = list[n+1];
+                    list[n+1] = tmp;
+                    swap = true;
+                }
+            }
+        }
+        return list;
+    }
+
+}
---
+++ src/test/java/juh/sorting/SortingAlgorithmTest.java
@@ -0,0 +1,68 @@
+package juh.sorting;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Random;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import juh.sorting.algorithm.BubbleSort;
+
+@RunWith(Parameterized.class)
+public class SortingAlgorithmTest {
+
+    private static final int MAX_INT = 100;
+
+    private static int[] array10 = new int[10];
+
+    private SortingAlgorithm algo;
+    private String algoName;
+
+    public SortingAlgorithmTest(SortingAlgorithm algo, String algoName) {
+        this.algo = algo;
+        this.algoName = algoName;
+    }
+
+    @BeforeClass
+    public static void initArray() throws Exception {
+        fill(array10);
+    }
+
+    private static void fill(int[] array) {
+        Random rand = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rand.nextInt(MAX_INT) + 1;
+        }
+        System.out.println("Random test array: " + Arrays.toString(array));
+    }
+
+    private static void assertSorted(int[] list) {
+        for (int i = 0; i < list.length - 1; i++) {
+            if (list[i] > list[i+1]) {
+                Assert.fail("array not sorted");
+            }
+        }
+    }
+
+    @Parameters(name = "{index}: testing {1}")
+    public static Collection algorithms() {
+        return Arrays.asList(new Object[][] {
+                {new BubbleSort(), "BubbleSort"},
+            });
+    }
+
+    @Test
+    public void testSort() {
+        int[] array = array10.clone();
+        algo.sort(array);
+        System.out.println("Sorted array (" + algoName + "): " +
+                           Arrays.toString(array));
+        assertSorted(array);
+    }
+
+}
